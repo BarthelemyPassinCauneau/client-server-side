2e42dfab7fd335dbcf3037b7757a48988ce5719a
Florent ROBERT <florent.robert@etu.unice.fr>
2021-02-08 19:14:36 +0100

    a second type of curve

--- client-side/src/app/home/home.component.ts
+++ client-side/src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../_services/backend/backend.service';
 // @ts-ignore
 import * as CanvasJS from '../../assets/canvasjs.min';
-
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -61,9 +61,73 @@ export class HomeComponent implements OnInit {
         ]
       }]
     });
+
+    let dataPoints: any[] = [];
+    let dpsLength = 0;
+    let curve = new CanvasJS.Chart("curveContainer",{
+      exportEnabled: true,
+      title:{
+        text:"Live Chart with Data-Points from External JSON"
+      },
+      data: [{
+        type: "spline",
+        dataPoints : dataPoints,
+      }]
+    });
+    
+    $.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=1&ystart=25&length=20&type=json&callback=?", function(data) {  
+      $.each(data, function(key, value){
+        dataPoints.push({x: value[0], y: parseInt(value[1])});
+      });
+      dpsLength = dataPoints.length;
+      chart.render();
+      updateChart();
+    });
+    function updateChart() {	
+    $.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=" + (dpsLength + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json&callback=?", function(data) {
+      $.each(data, function(key, value) {
+        dataPoints.push({
+        x: parseInt(value[0]),
+        y: parseInt(value[1])
+        });
+        dpsLength++;
+      });
       
+      if (dataPoints.length >  20 ) {
+            dataPoints.shift();				
+          }
+      
+      //setTimeout(function(){updateChart()}, 1000);
+    });
+
+    let dataPointss = [];
+    let y = 0;		
+    for ( var i = 0; i < 10000; i++ ) {		  
+      y += Math.round(5 + Math.random() * (-5 - 5));	
+      dataPointss.push({ y: y});
+    }
+    let curve2 = new CanvasJS.Chart("curve2Container", {
+      zoomEnabled: true,
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Performance Demo - 10000 DataPoints"
+      },
+      subtitles:[{
+        text: "Try Zooming and Panning"
+      }],
+      data: [
+      {
+        type: "line",                
+        dataPoints: dataPoints
+      }]
+    });
+      
+    curve.render();
+    curve2.render();
     cheese.render();
     chart.render();
+    }
   }
 
   sendServer(something: any){

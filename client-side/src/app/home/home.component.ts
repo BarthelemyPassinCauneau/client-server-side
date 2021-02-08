d3122f011ba74f58a6e41b97637b58e98d2b55d5
Florent ROBERT <florent.robert@etu.unice.fr>
2021-02-08 19:05:24 +0100

    cheese & chart

--- client-side/src/app/home/home.component.ts
+++ client-side/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../_services/backend/backend.service';
+// @ts-ignore
+import * as CanvasJS from '../../assets/canvasjs.min';
+
 
 @Component({
   selector: 'app-home',
@@ -13,6 +16,54 @@ export class HomeComponent implements OnInit {
   constructor(private back: BackendService) { }
 
   ngOnInit(): void {
+    let chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Basic Column Chart in Angular"
+      },
+      data: [{
+        type: "column",
+        dataPoints: [
+          { y: 71, label: "Apple" },
+          { y: 55, label: "Mango" },
+          { y: 50, label: "Orange" },
+          { y: 65, label: "Banana" },
+          { y: 95, label: "Pineapple" },
+          { y: 68, label: "Pears" },
+          { y: 28, label: "Grapes" },
+          { y: 34, label: "Lychee" },
+          { y: 14, label: "Jackfruit" }
+        ]
+      }]
+    });
+
+    let cheese = new CanvasJS.Chart("cheeseContainer", {
+      theme: "light2",
+      animationEnabled: true,
+      exportEnabled: true,
+      title:{
+        text: "Monthly Expense"
+      },
+      data: [{
+        type: "pie",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+        indexLabel: "{name} - #percent%",
+        dataPoints: [
+          { y: 450, name: "Food" },
+          { y: 120, name: "Insurance" },
+          { y: 300, name: "Traveling" },
+          { y: 800, name: "Housing" },
+          { y: 150, name: "Education" },
+          { y: 150, name: "Shopping"},
+          { y: 250, name: "Others" }
+        ]
+      }]
+    });
+      
+    cheese.render();
+    chart.render();
   }
 
   sendServer(something: any){

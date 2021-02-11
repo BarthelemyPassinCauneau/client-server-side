b7a8b50e634c011c164b0cbcab85cf7dd31ae04c
Theo-Fafet <tfafet@gmail.com>
2021-02-11 11:16:44 +0100

    build : setup routing

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,17 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import {GraphColumn} from './GraphColumn.js';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Coucou
-        </p>       
-        <GraphColumn></GraphColumn>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Coucou
+          </p>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/graph">Graph</Link>
+            </li>
+          </ul>
+          
+          <Switch>
+            <Route path="/graph">
+              <GraphColumn></GraphColumn>
+            </Route>
+          </Switch>
+
+        </header>
+      </div>
+    </Router>
   );
 }
 

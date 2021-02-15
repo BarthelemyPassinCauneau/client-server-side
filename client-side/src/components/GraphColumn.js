2c336ba2f888957e5b1a5b52550bd06d5dc7a169
Florent ROBERT <florent.robert@etu.unice.fr>
2021-02-15 18:58:07 +0100

    graph column with dropdown selection sem & dep

--- client-side/src/components/GraphColumn.js
+++ client-side/src/components/GraphColumn.js
@@ -1,23 +1,54 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import CanvasJSReact from '../assets/canvasjs/canvasjs.react';
+import Select from 'react-select'
+
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
- 
+const semaine = [
+	{ value: '22', label: '22' },
+	{ value: '23', label: '23' },
+	{ value: '24', label: '24' }
+]
+const departement = [
+	{ value: '04', label: '04' },
+	{ value: '05', label: '05' },
+	{ value: '06', label: '06' }
+] 
+
 export class GraphColumn extends Component {	
 	constructor(props) {
 		super(props);
-		this.state = { zero6 : [{key: 0}]};
+		this.state = { zero6 : [{key: 0}], defaultSemaine : {value : "24", label : "24"}, defaultDepartement : {value : "06", label : "06"}};
 	}
 
-	callServer06(){
-		fetch("http://localhost:8080/covid_data/heb/dep?week=2020-S24&dep=06")
+	componentDidMount() {
+		this.callServerUser("24", "06")
+	}
+
+	callServerUser(sem, dep){
+		let path = "http://localhost:8080/covid_data/heb/dep?week=2020-S"+sem+"&dep="+dep
+		console.log(path);
+		fetch(path)
 		.then(res => res.json())
 		.then(res => this.setState({ zero6: res}));
 	}
 
+	handleChangeSem = defaultSemaine => {
+		console.log(defaultSemaine.value);
+		this.callServerUser(defaultSemaine.value, this.state.defaultDepartement.value);
+		this.setState({ defaultSemaine });
+	};
+
+	handleChangeDep = defaultDepartement => {
+		this.callServerUser(this.state.defaultSemaine.value, defaultDepartement.value);
+		this.setState({ defaultDepartement });
+	};
+
 	render() {
 		var options = {}
-		
+		var {selectedSem} = this.state.defaultSemaine.value;
+		var {selectedDep} = this.state.defaultDepartement.value;
+		console.log(selectedSem);
 		if(this.state.zero6.length > 1 && this.state.zero6 != undefined){
 			options = {
 				animationEnabled: true,
@@ -47,33 +78,34 @@ export class GraphColumn extends Component {
 						{ x: this.state.zero6[10].cl_age90, y: this.state.zero6[10].P }
 					]
 				}]
-			} 
-			} else {
-				options = {
-					animationEnabled: true,
-					exportEnabled: true,
-					theme: "light2", //"light1", "dark1", "dark2"
-					title:{
-						text: "Simple Column Chart with Index Labels"
-					},
-					axisY: {
-						includeZero: true
-					},
-					data: [{
-						type: "column",
-						indexLabelFontColor: "#5A5757",
-						indexLabelPlacement: "outside",
-						dataPoints: [
-							{ x: 1, y: 1 }
-						]
-					}]
-				}
 			}
-			
-		this.callServer06();
-
+		} else {
+			options = {
+				animationEnabled: true,
+				exportEnabled: true,
+				theme: "light2", //"light1", "dark1", "dark2"
+				title:{
+					text: "Simple Column Chart with Index Labels"
+				},
+				axisY: {
+					includeZero: true
+				},
+				data: [{
+					type: "column",
+					indexLabelFontColor: "#5A5757",
+					indexLabelPlacement: "outside",
+					dataPoints: [
+						{ x: 1, y: 1 }
+					]
+				}]
+			}
+		}
 		return (
 		<div>
+			<p>Sélectionnez une semaine</p>
+			<Select options={semaine} value={selectedSem} onChange={this.handleChangeSem} defaultValue = {this.state.defaultSemaine} />
+			<p>Sélectionnez un département</p>
+			<Select options={departement} value={selectedDep} onChange={this.handleChangeDep} defaultValue = {this.state.defaultDepartement}/>
 			<CanvasJSChart options = {options} 
 				/* onRef={ref => this.chart = ref} */
 			/>

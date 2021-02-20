22e76d4bc55c79a2483169a479258f0050a03181
Florent ROBERT <florent.robert@etu.unice.fr>
2021-02-20 16:59:58 +0100

    refactor : graph class are now graph const

--- client-side/src/components/GraphColumn.js
+++ client-side/src/components/GraphColumn.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component, useState } from 'react';
 import { render } from "react-dom";
 import CanvasJSReact from '../assets/canvasjs/canvasjs.react';
 import Select from 'react-select'
@@ -6,121 +6,112 @@ import { GraphCurve } from "./GraphCurve";
 import { initialiseAgGridWithAngular1 } from "ag-grid-community";
 
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-const semaine = [
-	{ value: '22', label: '22' },
-	{ value: '23', label: '23' },
-	{ value: '24', label: '24' }
-]
-const departement = [
-	{ value: '04', label: '04' },
-	{ value: '05', label: '05' },
-	{ value: '06', label: '06' }
-] 
+export const GraphColumn = ({currentDep}) => {	
+	const [data, setData] = useState({ dataBack : [{key: 0}]});
+	const [departement, setDepartement] = useState({ defaultDepartement : {value : "00", label : "00"}});
+	const [semaine, setSemaine] = useState({ defaultSemaine : {value : "24", label : "24"}});
+	const semaineBase = [
+		{ value: '22', label: '22' },
+		{ value: '23', label: '23' },
+		{ value: '24', label: '24' }
+	]
+	const departementBase = [
+		{ value: '04', label: '04' },
+		{ value: '05', label: '05' },
+		{ value: '06', label: '06' }
+	] 
 
-export class GraphColumn extends Component {	
-	constructor(props) {
-		super(props);
-		this.state = { dataBack : [{key: 0}], defaultSemaine : {value : "24", label : "24"}, defaultDepartement : {value : "00", label : "00"}};
-	}
-
-	callServerUser(sem, dep){
+	const callServerUser = (sem, dep) => {
 		let path = "http://localhost:8080/covid_data/heb/dep?week=2020-S"+sem+"&dep="+dep
 		fetch(path)
 		.then(res => res.json())
-		.then(res => this.setState({ dataBack: res}));
+		.then(res => setData({ dataBack: res}));
 	}
 
-	handleChangeSem = defaultSemaine => {
-		this.callServerUser(defaultSemaine.value, this.state.defaultDepartement.value);
-		this.setState({ defaultSemaine });
-		this.setState({ defaultSemaine });
+	const handleChangeSem = defaultSemaine => {
+		callServerUser(defaultSemaine.value, departement.defaultDepartement.value);
+		setSemaine({ defaultSemaine });
 	};
 
-	handleChangeDep = defaultDepartement => {
-		this.callServerUser(this.state.defaultSemaine.value, defaultDepartement.value);
-		this.setState({ defaultDepartement });
-		this.setState({ defaultDepartement });
+	const handleChangeDep = defaultDepartement => {
+		callServerUser(semaine.defaultSemaine.value, defaultDepartement.value);
+		setDepartement({ defaultDepartement });
 	};
 
-	Init(){
-		if(this.state.defaultDepartement.value == "00" && this.props.currentDep != 0){
-			console.log(this.props.currentDep)
-			this.callServerUser("24", this.props.currentDep)
-			this.state.defaultDepartement.value = this.props.currentDep;
-			this.state.defaultDepartement.label = this.props.currentDep;
+	if(departement.defaultDepartement.value == "00" && currentDep != 0){
+		callServerUser("24", currentDep)
+		departement.defaultDepartement.value = currentDep;
+		departement.defaultDepartement.label = currentDep;
+	}
+
+	var options = {}
+	var {selectedSem} = semaine.defaultSemaine.value;
+	var {selectedDep} = departement.defaultDepartement.value;
+	if(data.dataBack.length > 1 && data.dataBack != undefined){
+		options = {
+			animationEnabled: true,
+			exportEnabled: true,
+			theme: "white2", 
+			title:{
+				text: "Nombre d'admission par tranche d'âge dans le département "+departement.defaultDepartement.value+" lors de la semaine "+semaine.defaultSemaine.value
+			},
+			axisY: {
+				title: "Nb admission"
+			},
+			axisX: {
+				title: "Tranche d'âge",
+				includeZero: true
+			},
+			data: [{
+				type: "column",
+				indexLabelFontColor: "#5A5757",
+				indexLabelPlacement: "outside",
+				dataPoints: [
+					{ x: data.dataBack[0].cl_age90, y: data.dataBack[0].P },
+					{ x: data.dataBack[1].cl_age90, y: data.dataBack[1].P },
+					{ x: data.dataBack[2].cl_age90, y: data.dataBack[2].P },
+					{ x: data.dataBack[3].cl_age90, y: data.dataBack[3].P },
+					{ x: data.dataBack[4].cl_age90, y: data.dataBack[4].P },
+					{ x: data.dataBack[5].cl_age90, y: data.dataBack[5].P },
+					{ x: data.dataBack[6].cl_age90, y: data.dataBack[6].P },
+					{ x: data.dataBack[7].cl_age90, y: data.dataBack[7].P },
+					{ x: data.dataBack[8].cl_age90, y: data.dataBack[8].P },
+					{ x: data.dataBack[9].cl_age90, y: data.dataBack[9].P },
+					{ x: data.dataBack[10].cl_age90, y: data.dataBack[10].P }
+				]
+			}]
+		}
+	} else {
+		options = {
+			animationEnabled: true,
+			exportEnabled: true,
+			theme: "white2",
+			title:{
+				text: "Nombre d'admission par tranche d'âge"
+			},
+			axisY: {
+				includeZero: true
+			},
+			data: [{
+				type: "column",
+				indexLabelFontColor: "#5A5757",
+				indexLabelPlacement: "outside",
+				dataPoints: [
+					{ x: 1, y: 1 }
+				]
+			}]
 		}
 	}
 
-	render() {
-		this.Init();
-		var options = {}
-		var {selectedSem} = this.state.defaultSemaine.value;
-		var {selectedDep} = this.state.defaultDepartement.value;
-		if(this.state.dataBack.length > 1 && this.state.dataBack != undefined){
-			options = {
-				animationEnabled: true,
-				exportEnabled: true,
-				theme: "white2", 
-				title:{
-					text: "Nombre d'admission par tranche d'âge dans le département "+this.state.defaultDepartement.value+" lors de la semaine "+this.state.defaultSemaine.value
-				},
-				axisY: {
-					title: "Nb admission"
-				},
-				axisX: {
-					title: "Tranche d'âge",
-					includeZero: true
-				},
-				data: [{
-					type: "column",
-					indexLabelFontColor: "#5A5757",
-					indexLabelPlacement: "outside",
-					dataPoints: [
-						{ x: this.state.dataBack[0].cl_age90, y: this.state.dataBack[0].P },
-						{ x: this.state.dataBack[1].cl_age90, y: this.state.dataBack[1].P },
-						{ x: this.state.dataBack[2].cl_age90, y: this.state.dataBack[2].P },
-						{ x: this.state.dataBack[3].cl_age90, y: this.state.dataBack[3].P },
-						{ x: this.state.dataBack[4].cl_age90, y: this.state.dataBack[4].P },
-						{ x: this.state.dataBack[5].cl_age90, y: this.state.dataBack[5].P },
-						{ x: this.state.dataBack[6].cl_age90, y: this.state.dataBack[6].P },
-						{ x: this.state.dataBack[7].cl_age90, y: this.state.dataBack[7].P },
-						{ x: this.state.dataBack[8].cl_age90, y: this.state.dataBack[8].P },
-						{ x: this.state.dataBack[9].cl_age90, y: this.state.dataBack[9].P },
-						{ x: this.state.dataBack[10].cl_age90, y: this.state.dataBack[10].P }
-					]
-				}]
-			}
-		} else {
-			options = {
-				animationEnabled: true,
-				exportEnabled: true,
-				theme: "white2",
-				title:{
-					text: "Nombre d'admission par tranche d'âge"
-				},
-				axisY: {
-					includeZero: true
-				},
-				data: [{
-					type: "column",
-					indexLabelFontColor: "#5A5757",
-					indexLabelPlacement: "outside",
-					dataPoints: [
-						{ x: 1, y: 1 }
-					]
-				}]
-			}
-		}
-		return (
+	return (
 		<div>
 			<p>Sélectionnez une semaine</p>
-			<Select options={semaine} value={selectedSem} onChange={this.handleChangeSem} defaultValue = {this.state.defaultSemaine} />
+			<Select options={semaineBase} value={selectedSem} onChange={handleChangeSem} defaultValue = {semaine.defaultSemaine} />
 			<p>Sélectionnez un département</p>
-			<Select options={departement} value={selectedDep} onChange={this.handleChangeDep} defaultValue = {this.state.defaultDepartement}/>
+			<Select options={departementBase} value={selectedDep} onChange={handleChangeDep} defaultValue = {departement.defaultDepartement}/>
 			<CanvasJSChart options = {options} />
 			<br></br>
-			<GraphCurve dep={this.state.defaultDepartement.value}/>
+			<GraphCurve currentDep={departement.defaultDepartement.value}/>
 		</div>
-		);
-	}
+	);
 }
--- client-side/src/components/GraphCurve.js
+++ client-side/src/components/GraphCurve.js
@@ -1,82 +1,81 @@
-import React, { Component } from "react";
+import { Component, useState } from 'react';
 import { render } from "react-dom";
 import CanvasJSReact from '../assets/canvasjs/canvasjs.react';
 
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 var dataPoints =[];
 var dep = 0;
-export class GraphCurve extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { dataBack : [{key: 0}], defaultSemaine : {value : "24", label : "24"}, defaultDepartement : {value : "06", label : "06"}};
-	}
+export const GraphCurve = ({currentDep}) => {
+	const [data, setData] = useState({ dataBack : [{key: 0}]});
 
-	callServerUser(){
+	const callServerUser = () =>{
 		dataPoints = [];
-		dep = this.props.dep;
+		dep = currentDep;
 		let path = "http://localhost:8080/covid_data/heb/dep?dep="+dep;
 		fetch(path)
 		.then(res => res.json())
 		.then(res => { 
-			this.setState({ dataBack: res});
-			this.state.dataBack.forEach(element => {
+			res.forEach(element => {
+				console.log(element);
 				if(element.cl_age90 == 0){
+					
 					dataPoints.push({ x: parseInt(element.week.split("S")[1], 10), y: element.P });
 				}
 			});
 			dataPoints.sort((a, b)=> a.x - b.x);
-			this.setState({dataBack : [{key: 0}]});
+			setData({dataBack : [{key: 0}]});
 		});
 	}
 
-	render() {	
-		var options = {}
-		if(dataPoints != [] ){
-			options = {
-				animationEnabled: true,
-				exportEnabled: true,
-				theme: "white2",
-				title: {
-					text: "Evolution du nombre d'admission pour le département "+this.props.dep,
-				},
-				axisX: {
-					title: "Semaine"
-				},
-				axisY: {
-					title: "Nb admission"
-				},
-				data: [{
-					type: "line",
-					dataPoints: dataPoints
-				}]
-			}
-		} else {
-			options = {
-				animationEnabled: true,
-				exportEnabled: true,
-				theme: "white2",
-				title: {
-					text: "Evolution du nombre d'admission pour le département "+this.props.dep,
-				},
-				axisX: {
-					title: "Semaine"
-				},
-				axisY: {
-					title: "Nb admission"
-				},
-				data: [{
-					type: "line",
-					dataPoints: {x:0, y:0}
-				}]
-			}
+	var options = {}
+	if(dataPoints != [] ){
+		options = {
+			animationEnabled: true,
+			exportEnabled: true,
+			theme: "white2",
+			title: {
+				text: "Evolution du nombre d'admission pour le département "+currentDep,
+			},
+			axisX: {
+				title: "Semaine"
+			},
+			axisY: {
+				title: "Nb admission"
+			},
+			data: [{
+				type: "line",
+				dataPoints: dataPoints
+			}]
 		}
-		if(dep != this.props.dep){
-			this.callServerUser();
+	} else {
+		options = {
+			animationEnabled: true,
+			exportEnabled: true,
+			theme: "white2",
+			title: {
+				text: "Evolution du nombre d'admission pour le département "+currentDep,
+			},
+			axisX: {
+				title: "Semaine"
+			},
+			axisY: {
+				title: "Nb admission"
+			},
+			data: [{
+				type: "line",
+				dataPoints: {x:0, y:0}
+			}]
 		}
-		return (
+	}
+
+	if(dep != currentDep){
+		callServerUser();
+	}
+
+
+	return (
 		<div>
 			<CanvasJSChart options = {options} />
 		</div>
-		);
-	}
+	);
 }

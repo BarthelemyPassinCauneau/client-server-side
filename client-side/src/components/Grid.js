13bf7be7732cc3708b8337c88cf30beb5b896b84
BarthelemyPassinCauneau <barthelemy.passin-cauneau@etu.unice.fr>
2021-02-23 00:51:27 +0100

    fix : warning grid

--- client-side/src/components/Grid.js
+++ client-side/src/components/Grid.js
@@ -7,21 +7,23 @@ export const Grid = ({data, mode}) => {
                 <table>
                     <thead>
                         <tr>
-                            <th colspan="2">Données en France datant du {new Intl.DateTimeFormat('fr-FR', {day: 'numeric', month: 'long', year: 'numeric'}).format(Date.UTC(parseInt(data[0].date.split("-")[0]), parseInt(data[0].date.split("-")[1])-1, parseInt(data[0].date.split("-")[2])))}</th>
+                            <th colSpan="2">Données en France datant du {new Intl.DateTimeFormat('fr-FR', {day: 'numeric', month: 'long', year: 'numeric'}).format(Date.UTC(parseInt(data[0].date.split("-")[0]), parseInt(data[0].date.split("-")[1])-1, parseInt(data[0].date.split("-")[2])))}</th>
                         </tr>
                     </thead>
-                    <tr>
-                        <td>Guéris</td>
-                        <td>{`${new Intl.NumberFormat('fr-FR').format(data[0].gueris)}`}</td>
-                    </tr>
-                    <tr>
-                        <td>Décès</td>
-                        <td>{`${new Intl.NumberFormat('fr-FR').format(data[0].deces)}`}</td>
-                    </tr>
-                    <tr>
-                        <td>Hospitalisés</td>
-                        <td>{`${new Intl.NumberFormat('fr-FR').format(data[0].hospitalises)}`}</td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <td>Guéris</td>
+                            <td>{`${new Intl.NumberFormat('fr-FR').format(data[0].gueris)}`}</td>
+                        </tr>
+                        <tr>
+                            <td>Décès</td>
+                            <td>{`${new Intl.NumberFormat('fr-FR').format(data[0].deces)}`}</td>
+                        </tr>
+                        <tr>
+                            <td>Hospitalisés</td>
+                            <td>{`${new Intl.NumberFormat('fr-FR').format(data[0].hospitalises)}`}</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         );
@@ -29,6 +31,6 @@ export const Grid = ({data, mode}) => {
     return (
     <div className={`Grid ${mode ? 'dark' : 'light'}`}>
         <p>Accès aux données en cours...</p>
-        <div class="loader"></div>
+        <div className="loader"></div>
     </div>);
 };

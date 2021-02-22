1da26cfadfcb4976524057ed3bee82f496b9a8b1
BarthelemyPassinCauneau <barthelemy.passin-cauneau@etu.unice.fr>
2021-02-23 00:29:10 +0100

    build : responsive grid and darkmode button

--- client-side/src/components/Home.js
+++ client-side/src/components/Home.js
@@ -2,10 +2,10 @@ export const Home = () => {
     return (
         <div>
             <h2>Bienvenue sur notre site de statistiques sur la Covid-19 !</h2>
-            <p>Vous pouvez naviguer à travers les différents onglets pour analyser les données recueillies en France durant cette épidémie.</p>
+            <p>Vous pouvez naviguer à travers les différents onglets pour consulter les données recueillies en France durant cette épidémie.</p>
             <h3>Sources :</h3>
             <ul>
-                <li>Données en direct (tableau en haut à gauche) : OpenCOVID19-fr</li>
+                <li>Données du tableau : OpenCOVID19-fr</li>
                 <li>Données des graphes et de la carte : Système d’Informations de DEPistage (SI-DEP)</li>
             </ul>
         </div>
